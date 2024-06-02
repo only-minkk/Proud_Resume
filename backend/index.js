e8f55d45267ee0f9bcd3919a0b2d16e6fc56135d
minkyu <tour00389@gmail.com>
2024-06-02 18:09:16 +0900

    feat : add dotenv and connect to MongoDB
    - Implemented dotenv for environment variable management
    - Added connectDB function to handle MongoDB connections

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,12 @@
 import app from "./src/app.js";
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const PORT = process.env.SEVER_PORT || 5005;
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Hello, this is back");
