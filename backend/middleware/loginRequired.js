582660b593f5d96ce5d45ba7c1c2335e7c3dc71c
minkyu <tour00389@gmail.com>
2024-06-04 22:29:58 +0900

    feat : Implement loginRequired middleware to authenticate logged in users.

---
+++ backend/middleware/loginRequired.js
@@ -0,0 +1,39 @@
+import jwt from "jsonwebtoken";
+
+const loginRequired = (req, res, next) => {
+  try {
+    const userToken = req.headers["authorization"]?.split(" ")[1] ?? null;
+
+    if (!userToken || userToken === "null") {
+      console.log(
+        "서비스 사용 요청이 있습니다. 하지만, Authorization 토큰 : 없음"
+      );
+
+      throw new Error(`{
+            result : "forbidden-approach",
+            reason : "회원만 사용할 수 있는 서비스입니다."
+        }`);
+    }
+
+    const secretKey = process.env.JWT_SECRET_KEY;
+
+    const jwtDecoded = jwt.verify(userToken, secretKey);
+
+    req.userId = jwtDecoded.id;
+    next();
+  } catch (error) {
+    throw new Error(error);
+    // throw new Error(
+    //   `name: ${error.name},
+    //   message: ${error.message},
+    //   expiredAt: ${error.expiredAt}`
+    // );
+    // throw new error(`{
+    //   name: error.name,
+    //   message: error.message,
+    //   expiredAt: error.expiredAt,
+    // }`);
+  }
+};
+
+export default loginRequired;
--- backend/src/api/routes/userRoutes.js
+++ backend/src/api/routes/userRoutes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import loginRequired from "../../../middleware/loginRequired.js";
 import { getUsers, updateUser } from "../controllers/userController.js";
 
 const userRouter = Router();
 
+userRouter.use(loginRequired);
 userRouter.get("/users", getUsers);
 userRouter.put("/users/:id", updateUser);
 
