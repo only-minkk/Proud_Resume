387c3171bc82d1806ddbab0e90aa0bce7efde662
minkyu <tour00389@gmail.com>
2024-06-04 22:53:21 +0900

    refactor : Relocate the loginRequired middleware from userRoutes.js to app.js.

--- backend/src/api/routes/userRoutes.js
+++ backend/src/api/routes/userRoutes.js
@@ -1,10 +1,8 @@
 import { Router } from "express";
-import loginRequired from "../../../middleware/loginRequired.js";
 import { getUsers, updateUser } from "../controllers/userController.js";
 
 const userRouter = Router();
 
-userRouter.use(loginRequired);
 userRouter.get("/users", getUsers);
 userRouter.put("/users/:id", updateUser);
 
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 import express from "express";
 import authRouter from "./api/routes/authRoutes.js";
+import loginRequired from "../middleware/loginRequired.js";
 import userRouter from "./api/routes/userRoutes.js";
 
 const app = express();
 app.use(express.json());
 app.use(authRouter);
+app.use(loginRequired);
 app.use(userRouter);
 
 export default app;
