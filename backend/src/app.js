9452eae6b6fc517c970d6c577f8d9d1c1e72a503
minkyu <tour00389@gmail.com>
2024-06-05 17:28:50 +0900

    feat : Apply resumeRouter to app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,11 +2,15 @@ import express from "express";
 import authRouter from "./api/routes/authRoutes.js";
 import loginRequired from "../middleware/loginRequired.js";
 import userRouter from "./api/routes/userRoutes.js";
+import resumeRouter from "./api/routes/resumeRoutes.js";
 
 const app = express();
+
 app.use(express.json());
+
 app.use(authRouter);
 app.use(loginRequired);
 app.use(userRouter);
+app.use(resumeRouter);
 
 export default app;
