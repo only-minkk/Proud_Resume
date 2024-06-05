705baeb1eddfc66751a70536ed80a7429b12c9b5
minkyu <tour00389@gmail.com>
2024-06-05 16:01:39 +0900

    feat : Apply authorizeUser Middleware to userRoutes

--- backend/src/api/routes/userRoutes.js
+++ backend/src/api/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
+import { authorizeUser } from "../../../middleware/authorizeUser.js";
 import { getUsers, updateUser } from "../controllers/userController.js";
 
 const userRouter = Router();
 
 userRouter.get("/users", getUsers);
-userRouter.put("/users/:id", updateUser);
+userRouter.put("/users/:id", authorizeUser, updateUser);
 
 export default userRouter;
