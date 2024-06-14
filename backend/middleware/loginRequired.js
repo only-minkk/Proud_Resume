0d3b43acb413ef705964e5e30543e70ad9d626f6
minkyu <tour00389@gmail.com>
2024-06-14 17:31:48 +0900

    refactor : Enhance error handling in loginRequired middleware

--- backend/middleware/loginRequired.js
+++ backend/middleware/loginRequired.js
@@ -3,16 +3,11 @@ import jwt from "jsonwebtoken";
 const loginRequired = (req, res, next) => {
   try {
     const userToken = req.headers["authorization"]?.split(" ")[1] ?? null;
-
     if (!userToken || userToken === "null") {
-      console.log(
-        "서비스 사용 요청이 있습니다. 하지만, Authorization 토큰 : 없음"
-      );
-
-      throw new Error(`{
-            result : "forbidden-approach",
-            reason : "회원만 사용할 수 있는 서비스입니다."
-        }`);
+      res.status(500).json({
+        error: "The token could not be verified.",
+        message: "토큰을 확인할 수 없습니다.",
+      });
     }
 
     const secretKey = process.env.JWT_SECRET_KEY;
@@ -22,7 +17,31 @@ const loginRequired = (req, res, next) => {
     req.userId = jwtDecoded.id;
     next();
   } catch (error) {
-    res.status(401).send(error);
+    switch (error.name) {
+      case "TokenExpiredError":
+        res.status(401).json({
+          error: "Unauthorized",
+          message: "토큰이 만료되었습니다.",
+        });
+        break;
+      case "JsonWebTokenError":
+        res.status(401).json({
+          error: "Unauthorized",
+          message: "유효하지 않은 토큰입니다.",
+        });
+        break;
+      case "NotBeforeError":
+        res.status(401).json({
+          error: "Unauthorized",
+          message: "토큰이 아직 활성화되지 않았습니다.",
+        });
+        break;
+      default:
+        res.status(500).json({
+          error: "Internal Server Error",
+          message: "서버 처리 과정 중 오류가 발생했습니다.",
+        });
+    }
   }
 };
 
