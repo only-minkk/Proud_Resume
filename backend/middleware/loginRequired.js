4b648852e50fb8c1d17a2200e279e933ca9c3eb3
minkyu <tour00389@gmail.com>
2024-06-18 16:32:23 +0900

    refactor : Improve security and clarity of error messages in the loginRequired middleware.

--- backend/middleware/loginRequired.js
+++ backend/middleware/loginRequired.js
@@ -2,44 +2,54 @@ import jwt from "jsonwebtoken";
 
 const loginRequired = (req, res, next) => {
   try {
+    // 헤더에서 토큰 추츨(Authorization : Bearer ${token})
     const userToken = req.headers["authorization"]?.split(" ")[1] ?? null;
+
+    // 토큰이 없거나 null인경우 에러 반환
     if (!userToken || userToken === "null") {
-      res.status(500).json({
+      return res.status(500).json({
         error: "The token could not be verified.",
-        message: "토큰을 확인할 수 없습니다.",
+        message: "로그인이 필요합니다.",
       });
     }
 
+    // 토큰 검증
     const secretKey = process.env.JWT_SECRET_KEY;
-
     const jwtDecoded = jwt.verify(userToken, secretKey);
 
+    // req에 userId 추가
     req.userId = jwtDecoded.id;
+
     next();
   } catch (error) {
     switch (error.name) {
       case "TokenExpiredError":
         res.status(401).json({
           error: "Unauthorized",
-          message: "토큰이 만료되었습니다.",
+          // message: "토큰이 만료되었습니다.",
+          message: "세션이 만료되었습니다. 다시 로그인해 주세요.",
         });
         break;
       case "JsonWebTokenError":
         res.status(401).json({
           error: "Unauthorized",
-          message: "유효하지 않은 토큰입니다.",
+          // message: "유효하지 않은 토큰입니다.",
+          message: "로그인 정보가 유효하지 않습니다. 다시 로그인해 주세요.",
         });
         break;
       case "NotBeforeError":
         res.status(401).json({
           error: "Unauthorized",
-          message: "토큰이 아직 활성화되지 않았습니다.",
+          // message: "토큰이 아직 활성화되지 않았습니다.",
+          message:
+            "접근이 아직 허용되지 않았습니다. 잠시 후 다시 시도해 주세요.",
         });
         break;
       default:
         res.status(500).json({
           error: "Internal Server Error",
-          message: "서버 처리 과정 중 오류가 발생했습니다.",
+          message:
+            "처리 중 예상치 못한 오류가 발생했습니다. 잠시 후 다시 시도해 주세요.",
         });
     }
   }
