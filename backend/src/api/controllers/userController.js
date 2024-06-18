8c502642d8331928c231e4d805c136bafffa066e
minkyu <tour00389@gmail.com>
2024-06-18 17:20:47 +0900

    feat : Add password update feature , add comments and remove the variable declarations.
    - Removed the variable declaration and used it directly in function arguments.
    - Added comments to clarify the purpose of each code segment.

--- backend/src/api/controllers/userController.js
+++ backend/src/api/controllers/userController.js
@@ -1,25 +1,66 @@
 import User from "../models/userModel.js";
+import bcrypt from "bcrypt";
 
+// 개인정보 조회
 export const getUsers = async (req, res) => {
   try {
-    const users = await User.find();
-    req.json(users);
+    // 유저 정보 조회
+    const user = await User.find({ _id: req.userId });
+
+    // 유저가 존재하지 않다면 throw 에러
+    if (!user) {
+      console.log("존재하지 않는 유저입니다.");
+      throw new Error("존재하지 않는 유저입니다.");
+    }
+
+    // 유저 정보 반환.
+    res.status(200).json(user);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
 
+// 개인정보 수정
 export const updateUser = async (req, res) => {
   try {
-    const { email, newName } = req.body;
+    // 유저 조회
+    const user = await User.findOne({ _id: req.userId });
+    if (!user) {
+      console.log("존재하지 않는 유저입니다.");
+      throw new Error("존재하지 않는 유저입니다.");
+    }
 
-    const updatedUser = await User.findOneAndUpdate(
-      { email: email },
-      { name: newName }
+    // 유저의 해시된 비밀번호와 입력된 현재 비밀번호 비교
+    const comparePassword = await bcrypt.compare(
+      req.body.currentPassword,
+      user.password
     );
-    console.log(updatedUser);
-    res.status(200).json(updatedUser);
+
+    // 현재 비밀번호가 기존 비밀번호와 다르다면 에러
+    if (!comparePassword) {
+      console.log("기존 비밀번호를 다시 확인하여 주세요.");
+      return res.status(400).json({
+        error: "Invalid input value",
+        message: "기존 비밀번호를 다시 확인하여 주세요.",
+      });
+    }
+
+    // 새 비밀번호 해쉬화
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    // 개인정보 업데이트
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: user._id },
+      {
+        name: req.body.name,
+        password: hashedPassword,
+      }
+    );
+
+    // 수정 완료 응답 반환
+    console.log("수정완료", updatedUser);
+    res.status(200).json({ success: true, message: "업데이트 성공" });
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    res.status(400).json({ success: false, message: error.message });
   }
 };
