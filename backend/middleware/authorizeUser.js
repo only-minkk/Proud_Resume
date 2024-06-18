42a601cb7e62b5f988cd694ee655a05d3f5e1064
minkyu <tour00389@gmail.com>
2024-06-18 16:43:30 +0900

    refactor : Streamline authorizeUser middleware by using req.params.userId directly and enhance error messaging.

--- backend/middleware/authorizeUser.js
+++ backend/middleware/authorizeUser.js
@@ -1,14 +1,19 @@
 export const authorizeUser = (req, res, next) => {
   try {
-    const userId = req.params.id;
-    if (req.userId === userId) {
+    // 토큰 검증 후 추출한 userId와 파라미터에 입력한 userId가 같은지 검증
+    if (req.userId === req.params.userId) {
       next();
     } else {
-      res
-        .status(403)
-        .send("액세스가 거부되었습니다: 이 작업을 수행할 권한이 없습니다.");
+      return res.status(401).json({
+        error: "Unauthorized",
+        message: "로그인 정보가 유효하지 않습니다. 다시 로그인해 주세요.",
+      });
     }
   } catch (error) {
-    throw new Error(error);
+    res.status(500).json({
+      error: "Internal Server Error",
+      message:
+        "처리 중 예상치 못한 오류가 발생했습니다. 잠시 후 다시 시도해 주세요.",
+    });
   }
 };
